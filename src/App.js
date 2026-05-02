3e661fd0775fe20f880334a56cea5d2d9c26cbf6
mac <khadijatououmytraore@esp.sn>
2026-05-02 21:32:12 +0000

    Setup initial SenTransport avec Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header />
+      <main className="contenu">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenue ! Cette application vous aide à trouver
+          votre ligne de bus à Dakar.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </main>
     </div>
   );
 }
